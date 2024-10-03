ab336c4750c9c7f19577c4bc27ff462f42fcf871
Saymen930 <115408305+saymen930@users.noreply.github.com>
2024-10-03 18:28:18 +0400

    Update config.py

--- AylinRobot/config.py
+++ AylinRobot/config.py
@@ -1,33 +1,33 @@
-# @AylinRobot
-# Sahib @HuseynH
-# Repo Açığdısa İcazəsis Götürmə Oğlum
-# Reponu Satan Kodların Götürən Kimliyindən Aslı Olmayaraq Peysərdi
+# 
+# 
+# 
+# 
 
-# @AylinRobot
-# Sahib @HuseynH
-# Repo Açığdısa İcazəsis Götürmə Oğlum
+# 
+# 
+# 
 
 import os
 
 class Config:
 
-   API_ID = int(os.getenv("API_ID", "8953338"))
-   API_HASH = os.getenv("API_HASH", "fe21f223cb02d8f7c1cbda651f553a45")
-   BOT_TOKEN = os.getenv("BOT_TOKEN", "6")
-   BOT_USERNAME = os.environ.get("BOT_USERNAME", "dddd")
-   BOT_NAME = os.environ.get("BOT_NAME", "Fatime")   
-   OWNER_ID = int(os.environ.get("OWNER_ID","6799203666"))
-   OWNER_NAME = os.environ.get("OWNER_NAME", "huseynh") 
+   API_ID = int(os.getenv("API_ID", "16501912"))
+   API_HASH = os.getenv("API_HASH", "34138f0596a914d229f8bc1ab7794bc6")
+   BOT_TOKEN = os.getenv("BOT_TOKEN", "6999080991:AAFm-Sad9EBJuGYA_y8BWLKNyHSMtBmgLeA")
+   BOT_USERNAME = os.environ.get("BOT_USERNAME", "EsilaChatBot")
+   BOT_NAME = os.environ.get("BOT_NAME", "Esila Sohbet")   
+   OWNER_ID = int(os.environ.get("OWNER_ID","6153472412"))
+   OWNER_NAME = os.environ.get("OWNER_NAME", "debubluman") 
    GONDERME_TURU = bool(os.environ.get("GONDERME_TURU", "False"))
-   MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://Huseyn:Huseyn@cluster0.zelpu0o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002209683682"))
-   PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "fffff")
-   PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1002209683682"))
-   BAN_GROUP = int(os.environ.get("BAN_GROUP", "-1002209683682"))
-   HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", "05a017d5-e3e5-424d-941f-3e60645e3141")
-   ALIVE_NAME = os.environ.get("ALIVE_NAME", "HUSEYN")
-   CHANNEL = os.environ.get("CHANNEL", "huseynh")
-   SUPPORT = os.environ.get("SUPPORT", "huseynh")
+   MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://emir:emir@cluster0.c2npk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002036606687"))
+   PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "musics")
+   PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1002022150339"))
+   BAN_GROUP = int(os.environ.get("BAN_GROUP", "-1002036606687"))
+   HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", "HRKU-dde44331-5d75-4c7f-8437-c774c4e5503a")
+   ALIVE_NAME = os.environ.get("ALIVE_NAME", "Emir")
+   CHANNEL = os.environ.get("CHANNEL", "esilabotbilgilendirme")
+   SUPPORT = os.environ.get("SUPPORT", "sorundestekk")
    ALIVE_IMG = os.environ.get("ALIVE_IMG", "https://telegra.ph/file/f6c186e3c581a223856c4.mp4") 
    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/b2c2ed59a89933a384ae3.jpg")
    COMMAND_PREFIXES = list(os.environ.get("COMMAND_PREFIXES", "/ ! .").split())
