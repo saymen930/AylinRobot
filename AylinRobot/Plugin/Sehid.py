96251056763e196114743dcf3ee0b58401e23ced
Saymen930 <115408305+saymen930@users.noreply.github.com>
2024-10-03 15:34:23 +0400

    Update Sehid.py

--- AylinRobot/Plugin/Sehid.py
+++ AylinRobot/Plugin/Sehid.py
@@ -1,6 +1,6 @@
-# @AylinRobot
-# Sahib @HuseynH
-# Repo Açığdısa İcazəsis Götürmə Oğlum
+# 
+# 
+# Repo 
 
 import secrets, string, aiohttp
 from AylinRobot import AylinRobot as app
@@ -8,7 +8,7 @@ from Sehid import random_line
 from pyrogram.errors import FloodWait
 from pyrogram import Client, filters
 
-@app.on_message(filters.command(["sehid"]))
+@app.on_message(filters.command(["şehit"]))
 async def _(client, message):
 	await message.delete()
 	await message.reply_text(await random_line('Sehid/sehid.txt'))
