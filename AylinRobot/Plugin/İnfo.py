0412ed942ce15c32b8fe462f9abdfc841c92eec6
Saymen930 <115408305+saymen930@users.noreply.github.com>
2024-10-03 18:12:35 +0400

    Update İnfo.py

--- AylinRobot/Plugin/İnfo.py
+++ AylinRobot/Plugin/İnfo.py
@@ -1,6 +1,6 @@
-# @AylinRobot
-# Sahib @HuseynH
-# Repo Açığdısa İcazəsis Götürmə Oğlum
+# 
+# 
+# 
 
 import os, asyncio, time, shlex, requests, pyrogram
 from helpers.extract_user import extract_user, last_online
