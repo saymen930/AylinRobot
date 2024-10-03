9ea30824a61496bc12a8ab2cd56e84e2c33549c0
Saymen930 <115408305+saymen930@users.noreply.github.com>
2024-10-03 18:09:02 +0400

    Update İd.py

--- AylinRobot/Plugin/İd.py
+++ AylinRobot/Plugin/İd.py
@@ -1,6 +1,6 @@
-# @AylinRobot
-# Sahib @HuseynH
-# Repo Açığdısa İcazəsis Götürmə Oğlum
+# 
+# 
+# Repo 
 
 from AylinRobot import AylinRobot as app
 from pyrogram import Client, filters
@@ -14,9 +14,9 @@ def ids(_, message):
     reply = message.reply_to_message
     if reply:
         message.reply_text(
-            f"**Sənin ID**: `{message.from_user.id}`\n**{reply.from_user.first_name}'s ID**: `{reply.from_user.id}`\n**Söhbət ID**: `{message.chat.id}`"
+            f"**Senin ID**: `{message.from_user.id}`\n**{reply.from_user.first_name}'s ID**: `{reply.from_user.id}`\n**Grup ID**: `{message.chat.id}`"
         )
     else:
         message.reply(
-            f"**Sənin ID**: `{message.from_user.id}`\n**Söhbət ID**: `{message.chat.id}`"
+            f"**Senin ID**: `{message.from_user.id}`\n**Grup ID**: `{message.chat.id}`"
         )
