45236108e1bed9fb312048344827efdbe538796f
Saymen930 <115408305+saymen930@users.noreply.github.com>
2024-10-03 14:56:27 +0400

    Update Button.py

--- AylinRobot/Plugin/Button.py
+++ AylinRobot/Plugin/Button.py
@@ -5,12 +5,12 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 START_BUTTONS = InlineKeyboardMarkup(
 [[InlineKeyboardButton('📢 Kanal', url=f"https://t.me/{Config.CHANNEL}"),
-InlineKeyboardButton("💬 Söhbət Qrupu", url=f"https://t.me/{Config.SUPPORT}"),
+InlineKeyboardButton("💬 Sohbet Qrupu", url=f"https://t.me/{Config.SUPPORT}"),
 ],[
-InlineKeyboardButton('ℹ️ Bot Haqqında', callback_data='bh'),  
-InlineKeyboardButton('📚 Bot Əmrləri', callback_data='help'),
+InlineKeyboardButton('ℹ️ Bot Hakkında', callback_data='bh'),  
+InlineKeyboardButton('📚 Bot Komutları', callback_data='help'),
 ],[        
-InlineKeyboardButton('➕ Məni Qrupa Əlavə Et ➕', url=f"https://t.me/{Config.BOT_USERNAME}?startgroup=true"),
+InlineKeyboardButton('➕ Beni grupuna ekle ➕', url=f"https://t.me/{Config.BOT_USERNAME}?startgroup=true"),
 ],[                
 InlineKeyboardButton('👨🏻‍💻 Bot Sahibi',  url=f"https://t.me/{Config.OWNER_NAME}"),
 InlineKeyboardButton('🎧 Playlist', url=f"https://t.me/{Config.PLAYLIST_NAME}"),]])
@@ -18,52 +18,52 @@ InlineKeyboardButton('🎧 Playlist', url=f"https://t.me/{Config.PLAYLIST_NAME}"
 #### KÖMƏK BUTTONU
 
 HELP_BUTTONS = InlineKeyboardMarkup(
-[[InlineKeyboardButton('🎧 Musiqi', callback_data='musıc'),
+[[InlineKeyboardButton('🎧 Şarkı', callback_data='musıc'),
 InlineKeyboardButton('⭐ Telegraph', callback_data='tg')
 ],[
 InlineKeyboardButton('🎲 Oyunlar', callback_data='oyun'),
-InlineKeyboardButton('🇦🇿 Şəhidlər', callback_data='sehıd'),
+InlineKeyboardButton('🇦🇿 Şehitler', callback_data='sehıd'),
 ],[        
-InlineKeyboardButton('🌀 Əyləncə', callback_data='eylence'),
-InlineKeyboardButton('♾️ Əlavələr', callback_data='elave'),
+InlineKeyboardButton('🌀 Eğlence', callback_data='eylence'),
+InlineKeyboardButton('♾️ extra', callback_data='elave'),
 ],[
-InlineKeyboardButton('🔍 Axtarış', callback_data='axtar'),
-InlineKeyboardButton('🔔 Tagger', callback_data='tag'),    
+InlineKeyboardButton('🔍 Arama', callback_data='axtar'),
+InlineKeyboardButton('🔔 Etiket', callback_data='tag'),    
 ],[
-InlineKeyboardButton('👨🏻‍💻 Sahib Əmrləri', callback_data='sahib'),
+InlineKeyboardButton('👨🏻‍💻 Sahib Komutları', callback_data='sahib'),
 ],[    
-InlineKeyboardButton('✖️ Bağla', callback_data='close'),   
-InlineKeyboardButton('🔙 Geri Qayıt', callback_data='home'),]])
+InlineKeyboardButton('✖️ Kapat', callback_data='close'),   
+InlineKeyboardButton('🔙 Geri Dön', callback_data='home'),]])
 
 ### GERİ BUTTONU    
 
 MUSIC_BUTTONS = InlineKeyboardMarkup(
-[[InlineKeyboardButton('✖️ Bağla', callback_data='close'),   
-InlineKeyboardButton('🔙 Geri Qayıt', callback_data='help'),]])
+[[InlineKeyboardButton('✖️ Kapat', callback_data='close'),   
+InlineKeyboardButton('🔙 Geri Dön', callback_data='help'),]])
 TELEGRAPH_BUTTONS = InlineKeyboardMarkup(
-[[InlineKeyboardButton('✖️ Bağla', callback_data='close'),   
-InlineKeyboardButton('🔙 Geri Qayıt', callback_data='help'),]])
+[[InlineKeyboardButton('✖️ Kapat', callback_data='close'),   
+InlineKeyboardButton('🔙 Geri Dön', callback_data='help'),]])
 SEHID_BUTTONS = InlineKeyboardMarkup(
-[[InlineKeyboardButton('✖️ Bağla', callback_data='close'),   
-InlineKeyboardButton('🔙 Geri Qayıt', callback_data='help'),]])        
+[[InlineKeyboardButton('✖️ Kapat', callback_data='close'),   
+InlineKeyboardButton('🔙 Geri Dön', callback_data='help'),]])        
 OYUN_BUTTONS = InlineKeyboardMarkup(
-[[InlineKeyboardButton('✖️ Bağla', callback_data='close'),   
-InlineKeyboardButton('🔙 Geri Qayıt', callback_data='help'),]])
+[[InlineKeyboardButton('✖️ Kapat', callback_data='close'),   
+InlineKeyboardButton('🔙 Geri Dön', callback_data='help'),]])
 EYLENCE_BUTTONS = InlineKeyboardMarkup(
-[[InlineKeyboardButton('✖️ Bağla', callback_data='close'),   
-InlineKeyboardButton('🔙 Geri Qayıt', callback_data='help'),]])     
+[[InlineKeyboardButton('✖️ Kapat', callback_data='close'),   
+InlineKeyboardButton('🔙 Geri Dön', callback_data='help'),]])     
 SAHIB_BUTTONS = InlineKeyboardMarkup(
-[[InlineKeyboardButton('✖️ Bağla', callback_data='close'),   
-InlineKeyboardButton('🔙 Geri Qayıt', callback_data='help'),]])
+[[InlineKeyboardButton('✖️ Kapat', callback_data='close'),   
+InlineKeyboardButton('🔙 Geri Dön', callback_data='help'),]])
 ELAVE_BUTTONS = InlineKeyboardMarkup(
-[[InlineKeyboardButton('✖️ Bağla', callback_data='close'),   
-InlineKeyboardButton('🔙 Geri Qayıt', callback_data='help'),]])
+[[InlineKeyboardButton('✖️ Kapat', callback_data='close'),   
+InlineKeyboardButton('🔙 Geri Dön', callback_data='help'),]])
 AXTAR_BUTTONS = InlineKeyboardMarkup(
-[[InlineKeyboardButton('✖️ Bağla', callback_data='close'),   
-InlineKeyboardButton('🔙 Geri Qayıt', callback_data='help'),]])
+[[InlineKeyboardButton('✖️ Kapat', callback_data='close'),   
+InlineKeyboardButton('🔙 Geri Dön', callback_data='help'),]])
 TAGGER_BUTTONS = InlineKeyboardMarkup(
-[[InlineKeyboardButton('✖️ Bağla', callback_data='close'),   
-InlineKeyboardButton('🔙 Geri Qayıt', callback_data='help'),]])
+[[InlineKeyboardButton('✖️ Kapat', callback_data='close'),   
+InlineKeyboardButton('🔙 Geri Dön', callback_data='help'),]])
 BH_BUTTONS = InlineKeyboardMarkup(
-[[InlineKeyboardButton('✖️ Bağla', callback_data='close'),   
-InlineKeyboardButton('🔙 Geri Qayıt', callback_data='home'),]])
+[[InlineKeyboardButton('✖️ Kapat', callback_data='close'),   
+InlineKeyboardButton('🔙 Geri Dön', callback_data='home'),]])
